5a5a53a564bb2747e9c45885d1b722465d82c518
PRIYANSHU KUMAR JHA <priyanshukumarjha@Priyanshus-MacBook-Air.local>
2025-04-12 00:58:09 +0530

    removed wallet-connect component

--- src/components/walletconnect.tsx
+++
@@ -1,9 +0,0 @@
-import { Account } from './account'
-import { WalletOptions } from './wallet-options'
-import { useAccount } from 'wagmi'
-
-export default function WalletConnect() {
-  const { isConnected } = useAccount()
-  if (isConnected) return <Account />
-  return <WalletOptions />
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,38 @@
-import { useRouter } from 'next/router';
-import { useAnonAadhaar } from '@anon-aadhaar/react';
-import { useEffect } from 'react';
-import WalletConnect from "../components/walletconnect"
+import { useRouter } from "next/router";
+import { useAnonAadhaar } from "@anon-aadhaar/react";
+import { Account } from "../components/account";
+import { WalletOptions } from "../components/wallet-options";
+import { useAccount } from "wagmi";
+import { useEffect } from "react";
 
 export default function Home() {
   const [anonAadhaar] = useAnonAadhaar();
   const router = useRouter();
 
   useEffect(() => {
-    if (anonAadhaar.status === 'logged-in') {
-      router.push('/aadhaar');
+    if (anonAadhaar.status === "logged-in") {
+      router.push("/aadhaar");
     }
   }, [anonAadhaar.status, router]);
 
-  if (anonAadhaar.status === 'logged-in') {
+  if (anonAadhaar.status === "logged-in") {
     return <div>Redirecting to login...</div>;
   }
 
   return (
-    <div>
-      <h1>Welcome to the Bhendi!</h1>
-      <WalletConnect/>
+    <section className="h-[100vh] w-[100vw] bg-gray-100">
+      <h1 className="text-[10vh] text-center">Welcome to the Bhendi!</h1>
+      <div className="walletConnection flex flex-col w-[100vw] bg-slate-300 justify-center items-center">
+        <p className="text-[4vh]">Connect to wallet!</p>
+        <WalletConnect />
+      </div>
       <p>You're successfully logged in with AnonAadhaar!</p>
-    </div>
+    </section>
   );
+}
+
+function WalletConnect() {
+  const { isConnected } = useAccount();
+  if (isConnected) return <Account />;
+  return <WalletOptions />;
 }
